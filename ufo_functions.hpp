3d114dfcb20c5ba1b31552ca653f987376209358
Chidinma Mba <62717372+ChidinmaMba@users.noreply.github.com>
2023-01-06 19:16:20 -0500

    Create ufo_functions.hpp

---
+++ ufo_functions.hpp
@@ -0,0 +1,10 @@
+#include <vector>
+
+void greet();
+
+void end_game(std::string answer, std::string codeword);
+
+void display_status(std::vector<char> incorrect, std::string answer);
+
+void display_misses(int misses);
+
